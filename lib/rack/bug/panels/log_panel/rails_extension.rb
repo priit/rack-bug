2718b37b8eadcb580f30e11e9ec00e8ccee38030
tconnor <timocratic@gmail.com>
2009-04-25 13:27:56 -0700

    hook into any logger

--- lib/rack/bug/panels/log_panel/rails_extension.rb
+++ lib/rack/bug/panels/log_panel/rails_extension.rb
@@ -1,15 +1,21 @@
-if defined?(Rails) #&& Rails.logger
-  module LoggingExtensions
-    def self.included(target)
-      target.alias_method_chain :add, :rack_bug
-    end
-    
-    def add_with_rack_bug(*args, &block)
-      logged_message = add_without_rack_bug(*args, &block)
-      Rack::Bug::LogPanel.record(*args)
-      return logged_message
-    end
+module LoggingExtensions
+  def self.included(target)
+    target.alias_method_chain :add, :rack_bug
   end
-
-  ActiveSupport::BufferedLogger.send :include, LoggingExtensions
+  
+  def add_with_rack_bug(*args, &block)
+    logged_message = add_without_rack_bug(*args, &block)
+    Rack::Bug::LogPanel.record(*args)
+    return logged_message
+  end
 end
+
+if defined?(Rack::Bug::LoggerClass)
+  logger_klass = Rack::Bug::LoggerClass
+elsif defined?(ActiveSupport::BufferedLogger)
+  logger_klass = ActiveSupport::BufferedLogger
+elsif defined?(Logger)
+  logger_klass = Logger
+end
+
+logger_klass.send :include, LoggingExtensions if logger_klass
