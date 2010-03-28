51e38676dacbbddbdc499a675195106ee94f2f71
Tim Connor <timocratic@gmail.com>
2010-03-27 18:44:53 -0700

    move custom matchers to their own file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,12 +16,14 @@ end
 require "rack/bug"
 require "spec/fixtures/sample"
 require "spec/fixtures/dummy_panel"
+require "spec/custom_matchers"
 
 Spec::Runner.configure do |config|
   TIME_MS_REGEXP = /\d+\.\d{2}ms/
   
   config.include Rack::Test::Methods
   config.include Webrat::Matchers
+  config.include CustomMatchers
   
   config.before do
     # This allows specs to record data outside the request
@@ -33,25 +35,5 @@ Spec::Runner.configure do |config|
   
   def app
     Sample
-  end
-  
-  def have_row(container, key, value = nil)
-    simple_matcher("contain row") do |response|
-      if value
-        response.should have_selector("#{container} tr", :content => key) do |row|
-          row.should contain(value)
-        end
-      else
-        response.should have_selector("#{container} tr", :content => key)
-      end
-    end
-  end
-  
-  def have_heading(text)
-    simple_matcher("have heading") do |response|
-      response.should have_selector("#rack_bug_toolbar li") do |heading|
-        heading.should contain(text)
-      end
-    end
   end
 end
